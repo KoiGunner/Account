0afca8fd35bd0817c48613498dbb956c95eae020
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2024-10-22 17:46:12 -0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,48 +1,56 @@
-public class Account {
-    private final String name;
-    private final int number;
-    private double balance;
-
-    public Account(String name, int number, double balance){
-        this.name = name;
-        this.number = number;
-        this.balance = balance;
-    }
-
-    /* Método para depósito do valor */
-
-    public void deposit(double amount){
-        if (amount < 0){
-            throw new IllegalArgumentException("O valor precisa ser positivo");
-        } else {
-            balance += amount;
-        }
-    }
-
-    /* Método para saque do valor */
-
-    public void withdraw(double amount){
+public sealed class Account permits SavingsAccount {
+	
+	private final String nameOwner;
+	private final String emailOwner;
+	private final String numberId;
+	private double balance;
+	
+	private static int currentId = 1;
+	
+	//construtor
+	
+	public Account(String nameOwner, String emailOwner) {
+		this.nameOwner = nameOwner;
+		this.emailOwner = emailOwner;
+		this.numberId = "000" + currentId++;
+	}
+	
+	//Método para depósito
+	
+	public void deposit(double amount) {
+		if(amount < 0) {
+			throw new IllegalArgumentException("Não há crédito para depositar");
+		} 
+		balance += amount;
+	}
+	
+	//Método para saque
+	
+	public void withdraw(double amount){
         if (amount > 0 && amount <= balance){
             balance -= amount;
         } else {
         throw new IllegalArgumentException("Saque inválido ou sem saldo suficiente");
         }
-    }
-
-    /* Método para transferência de valor */
-
-    public void transfer(double amount, Account transferAccount){
-        if(transferAccount == null){
-            throw new IllegalArgumentException("Não há como efetuar, a conta é nula");
-        } if (amount <= 0 ){
-            throw new IllegalArgumentException("O valor precisa ser positivo");
-        }
-        withdraw(amount);
-        transferAccount.deposit(amount);
-    }
-
-    /* Método para impressão dos atributos */
-
-    public String toString(){
-        return String.format("{name: %s, number: %d, balance: 2.%f}", name, number, balance);
-    }
+	}
+	
+	//Método para transferência
+	
+	public void transfer(Account destinationAccount, double amount) {
+		if (destinationAccount == null) {
+			throw new IllegalArgumentException("É necessário informar uma conta");
+		} if (amount < 0 ) {
+			throw new IllegalArgumentException("É necessário ter saldo positivo");
+		} 
+		withdraw(amount);
+		destinationAccount.deposit(amount);
+	}
+	
+	//Exibição formatada ao usuário
+	
+	@Override
+	public String toString() {
+		DecimalFormat dc = new DecimalFormat("#.00");
+		return "Nome do titular: " + nameOwner + 
+				", E-mail do títular: " + emailOwner + ", Id do títular:  " + numberId + ", Saldo na conta: " + dc.format(balance);
+	}
